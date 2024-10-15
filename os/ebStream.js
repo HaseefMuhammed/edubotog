f5c99b66a6f50fb085884f45f4714b24da1843bb
Haseef Muhammed <haseefmuhammedpc@gmail.com>
2024-10-15 13:44:13 +0530

    disitrict level base tree

--- os/ebStream.js
+++ os/ebStream.js
@@ -1,8 +1,8 @@
 const APP_ID = "26615088d2834fa49c767edbb96c92da";
-const TOKEN = "007eJxTYFi07fMtle3G8drL7325Vv7y/Mw1J/5cbhd/vy9Y1GzXu8oyBQYjMzNDUwMLixQjC2OTtEQTy2RzM/PUlKQkS7NkS6OURNnpF9MaAhkZJO1jGBkZIBDEZ2HITczMY2AAAHhHImw=";
+const TOKEN = "007eJxTYGitlHvVIrOaae6Em8/8/Kb97a68r6NoMzma4cuO6CeTn15XYDAyMzM0NbCwSDGyMDZJSzSxTDY3M09NSUqyNEu2NEpJNJ/Plt4QyMhgNOUnMyMDBIL4LAy5iZl5DAwAjmcgPA==";
 const CHANNEL = "main";
 
-const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'});
+const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
 
 let localTracks = [];
 let remoteUsers = {};
@@ -46,23 +46,23 @@ let handleUserJoined = async (user, mediaType) => {
   remoteUsers[user.uid] = user
   await client.subscribe(user, mediaType)
 
-  if (mediaType === 'video'){
+  if (mediaType === 'video') {
     let player = document.getElementById(`user-container-${user.uid}`)
-    if (player != null){
+    if (player != null) {
       player.remove()
     }
     player = `<div class="video-container" id="user-container-${user.uid}">
         <div class="video-player" id="user-${user.uid}"></div>
         </div>`
 
-        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
 
-        user.videoTrack.play(`user-${user.uid}`)
+    user.videoTrack.play(`user-${user.uid}`)
   }
 
-    if (mediaType === 'audio'){
-      user.audioTrack.play()
-    }
+  if (mediaType === 'audio') {
+    user.audioTrack.play()
+  }
 
 }
 
@@ -72,7 +72,7 @@ let handleUserLeft = async (user) => {
 }
 
 let leaveAndRemoveLocalStream = async () => {
-  for(let i = 0; localTracks.length > i; i++){
+  for (let i = 0; localTracks.length > i; i++) {
     localTracks[i].stop()
     localTracks[i].close()
   }
@@ -84,11 +84,11 @@ let leaveAndRemoveLocalStream = async () => {
 }
 
 let toggleMic = async (e) => {
-  if (localTracks[0].muted){
+  if (localTracks[0].muted) {
     await localTracks[0].setMuted(false)
     e.target.innerText = 'Mic on'
     e.target.style.backgroundColor = 'cadetblue'
-  }else{
+  } else {
     await localTracks[0].setMuted(true)
     e.target.innerText = 'Mic off'
     e.target.style.backgroundColor = '#EE4B2B'
